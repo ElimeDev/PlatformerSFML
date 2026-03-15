fa350fa2779f41d333b54b8201e35e62b3fcc2d6
Emile Chabrillat <emile.chabrillat@gmail.com>
2026-03-15 14:43:40 +0100

    ajout du scrolling

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -5,9 +5,7 @@ using namespace sf;
 Game::Game(Vector2f size, std::string title)
     : m_player(sf::Vector2f(size.x/2, size.y/2)),
       m_window(VideoMode(size.x, size.y), title)
-{
-    
-}
+{ }
 
 Game::~Game()
 {
@@ -26,12 +24,16 @@ void Game::mainloop()
         float deltaTime = m_clock.restart().asSeconds();
 
         m_player.update(deltaTime, m_input);
+        handleCollision();
 
-		handleCollision();
+        // Mettre à jour la caméra pour suivre le joueur (vertical uniquement)
+        sf::View view = m_window.getDefaultView();
+        // Bloquer le scrolling latéral - garder la caméra centrée horizontalement
+        view.setCenter(400.f, m_player.getPosition().y);
+        m_window.setView(view);
 
         m_window.clear(Color::Black);
 
-        //Draw here
         // Draw platforms from the global map
         for (auto& p : platforms)
         {
--- src/data/map.cpp
+++ src/data/map.cpp
@@ -14,4 +14,56 @@ std::vector<Platform> platforms = {
     Platform(sf::Vector2f(520.f, 360.f), sf::Vector2f(120.f, 20.f)),
     Platform(sf::Vector2f(240.f, 300.f), sf::Vector2f(100.f, 20.f)),
     Platform(sf::Vector2f(600.f, 260.f), sf::Vector2f(140.f, 20.f)),
+
+    // Right side ascending path
+    Platform(sf::Vector2f(680.f, 380.f), sf::Vector2f(100.f, 20.f)),
+    Platform(sf::Vector2f(650.f, 320.f), sf::Vector2f(110.f, 20.f)),
+    Platform(sf::Vector2f(680.f, 250.f), sf::Vector2f(100.f, 20.f)),
+
+    // Left side mid-section platforms
+    Platform(sf::Vector2f(50.f, 380.f), sf::Vector2f(130.f, 20.f)),
+    Platform(sf::Vector2f(80.f, 280.f), sf::Vector2f(120.f, 20.f)),
+
+    // Central challenging section
+    Platform(sf::Vector2f(400.f, 240.f), sf::Vector2f(90.f, 18.f)),
+    Platform(sf::Vector2f(320.f, 180.f), sf::Vector2f(100.f, 20.f)),
+    Platform(sf::Vector2f(480.f, 180.f), sf::Vector2f(100.f, 20.f)),
+
+    // Top section - difficult jumps
+    Platform(sf::Vector2f(200.f, 120.f), sf::Vector2f(110.f, 18.f)),
+    Platform(sf::Vector2f(550.f, 130.f), sf::Vector2f(110.f, 18.f)),
+    Platform(sf::Vector2f(375.f, 70.f), sf::Vector2f(50.f, 15.f)),
+
+    // Very high section - left path
+    Platform(sf::Vector2f(100.f, 0.f), sf::Vector2f(130.f, 20.f)),
+    Platform(sf::Vector2f(50.f, -80.f), sf::Vector2f(120.f, 20.f)),
+    Platform(sf::Vector2f(150.f, -160.f), sf::Vector2f(100.f, 18.f)),
+    Platform(sf::Vector2f(80.f, -240.f), sf::Vector2f(110.f, 20.f)),
+
+    // Very high section - center path
+    Platform(sf::Vector2f(350.f, -20.f), sf::Vector2f(120.f, 20.f)),
+    Platform(sf::Vector2f(300.f, -100.f), sf::Vector2f(100.f, 18.f)),
+    Platform(sf::Vector2f(420.f, -100.f), sf::Vector2f(100.f, 18.f)),
+    Platform(sf::Vector2f(350.f, -180.f), sf::Vector2f(120.f, 20.f)),
+    Platform(sf::Vector2f(300.f, -260.f), sf::Vector2f(100.f, 18.f)),
+
+    // Very high section - right path
+    Platform(sf::Vector2f(650.f, -40.f), sf::Vector2f(130.f, 20.f)),
+    Platform(sf::Vector2f(700.f, -120.f), sf::Vector2f(110.f, 20.f)),
+    Platform(sf::Vector2f(600.f, -200.f), sf::Vector2f(120.f, 18.f)),
+    Platform(sf::Vector2f(700.f, -280.f), sf::Vector2f(100.f, 20.f)),
+
+    // Ultra high section - narrowing challenge
+    Platform(sf::Vector2f(350.f, -340.f), sf::Vector2f(100.f, 18.f)),
+    Platform(sf::Vector2f(250.f, -420.f), sf::Vector2f(80.f, 16.f)),
+    Platform(sf::Vector2f(450.f, -420.f), sf::Vector2f(80.f, 16.f)),
+    Platform(sf::Vector2f(350.f, -500.f), sf::Vector2f(60.f, 15.f)),
+    Platform(sf::Vector2f(300.f, -580.f), sf::Vector2f(70.f, 15.f)),
+    Platform(sf::Vector2f(420.f, -580.f), sf::Vector2f(70.f, 15.f)),
+
+    // Final peak section - extreme difficulty
+    Platform(sf::Vector2f(350.f, -660.f), sf::Vector2f(50.f, 14.f)),
+    Platform(sf::Vector2f(250.f, -740.f), sf::Vector2f(60.f, 15.f)),
+    Platform(sf::Vector2f(450.f, -740.f), sf::Vector2f(60.f, 15.f)),
+    Platform(sf::Vector2f(350.f, -820.f), sf::Vector2f(40.f, 12.f)),
 };
